e3cddf4a94c2741eda53671267bcea7efdfff556
zhe41223118 <luzhe859@gmail.com>
2025-03-21 14:53:10 +0800

    add task及分頁面task1-task6

--- init.py
+++ init.py
@@ -11,7 +11,7 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "cdproject"
-    ip = "127.0.0.1"
+    ip = "2001:288:6004:17:fff1:cd25:0:a024"
     dynamic_port = 9444
     static_port = 8444
     def __init__(self):
